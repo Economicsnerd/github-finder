31bead517ce62203fa9796ba7f3b52500393c266
Idriss Amraoui <idrissamraoui@protonmail.com>
2020-10-19 15:09:59 -0400

    Search functionality done

--- src/Components/Users/Search.js
+++ src/Components/Users/Search.js
@@ -7,7 +7,8 @@ export class Search extends Component {
 
   onSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state.text);
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
   };
   onChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
@@ -16,7 +17,7 @@ export class Search extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.onSubmit} className="form">
+        <form onSubmit={this.onSubmit.bind(this)} className="form">
           <input
             type="text"
             name="text"
