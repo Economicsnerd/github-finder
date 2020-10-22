d66320d9794d411480b1d8e045c49468fd31052d
Idriss Amraoui <idrissamraoui@protonmail.com>
2020-10-22 10:43:33 -0400

    App finalized. Need to refactor

--- src/Components/Users/User.js
+++ src/Components/Users/User.js
@@ -68,7 +68,7 @@ export class User extends Component {
               </Fragment>
             )}
             <a href={html_url} className="btn btn-dark my-1">
-              See on Github
+              See on Github <i class="fas fa-external-link-alt"></i>
             </a>
             <ul>
               <li>
